457a4e9c50ccea211616ac9c0a895348ad0428a2
暮雪流殇 <150791510+MuXueLiuShang@users.noreply.github.com>
2023-11-21 11:39:00 +0800

    Add files via upload

---
+++ C语言学习4/C语言学习4/源.cpp
@@ -0,0 +1,112 @@
+#include <stdio.h>
+int main()
+{
+
+	return 0;
+}
+
+
+
+
+//int main()
+//{
+//	int a = 1;
+//	while (a<=100)
+//	{
+//		printf("%d ", a);
+//		a += 2;
+//	}
+//	return 0;
+//}
+//{
+//	int a = 1;
+//	while (a<=100)
+//	{
+//		if (a%2==1)
+//			printf("%d ", a);
+//			a++;
+//	}
+//	return 0;
+//}
+
+//{
+//	int num = 0;
+//	scanf_s("%d", &num);
+//	if (num%2 == 1)
+//		printf("此数是奇数\n");
+//	else
+//		printf("此数是偶数\n");
+//	return 0;
+//}
+
+
+
+//{
+//	int num = 4;
+//	//if (num=5) =是赋值 ==是判断相等
+//	if (5==num)
+//	{
+//		printf("hehe\n");
+//	}
+//	return  0;
+//}
+
+//else匹配最近的未匹配的if进行匹配
+// 
+//{
+//	int age = 0;
+//	scanf_s("%d", &age);
+//	if (age < 18)
+//	{
+//		printf("未成年\n");
+//		printf("不能谈恋爱\n");
+//	}
+//	else
+//	{
+//		if (age >= 18 && age < 28)
+//			printf("青年\n");
+//		else if (age >= 28 && age < 50)
+//			printf("壮年\n");
+//		else if (age >= 50 && age < 90)
+//			printf("老年\n");
+//		else
+//			printf("老不死\n");
+//	}
+//	return 0;
+//}
+
+
+
+//{
+//	int age = 100;
+//	if (age < 18)
+//		printf("未成年\n");
+//	else
+//	{
+//		if (age >= 18 && age < 28)
+//			printf("青年\n");
+//		else if (age >= 28 && age < 50)
+//			printf("壮年\n");
+//		else if (age >= 50 && age < 90)
+//			printf("老年\n");
+//		else
+//			printf("老不死\n");
+//	}
+//	return 0;
+//
+//}
+//{
+//	int age = 100;
+//	if (age < 18)
+//		printf("未成年\n");
+//	else if (age >= 18 && age < 28)
+//		printf("青年\n");
+//	else if (age >= 28 && age < 50)
+//		printf("壮年\n");
+//	else if (age >= 50 && age < 90)
+//		printf("老年\n");
+//	else 
+//		printf("老不死\n");
+//
+//	return 0;
+//}
